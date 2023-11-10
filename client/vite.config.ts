2e02f3fd8ea3bb3d42a118f61b41106d1d4515a3
Abin273 <abinvarghese273@gmail.com>
2023-11-10 11:19:13 +0530

    vite host problem solved

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Change this to your desired port number
-  },
-})
+	plugins: [react()],
+	server: {
+		host: true,
+		port: 3000, // This is the port which we will use in docker
+		watch: {
+			usePolling: true
+		}
+	},
+	build: {
+		chunkSizeWarningLimit: 1600
+	},
+	preview: {
+		port: 3000
+	}
+});
