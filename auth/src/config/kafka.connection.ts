327573b40fb1d77212616402e7645455acb3239b
iamabin <iamabin1100@gmail.com>
2024-10-31 09:54:20 +0530

    feat: add retry in kafka config

--- auth/src/config/kafka.connection.ts
+++ auth/src/config/kafka.connection.ts
@@ -3,6 +3,11 @@ import { Kafka } from 'kafkajs';
 const kafkaClient = new Kafka({
     clientId: 'auth-client',
     brokers: ['devhive-kafka:9092'],
+    retry: {
+        retries: 5, // Number of retries
+        initialRetryTime: 300, // Initial time to wait before the first retry (in ms)
+        maxRetryTime: 10000, // Maximum time to wait before giving up (in ms)
+      }
 });
 
 export { kafkaClient };
